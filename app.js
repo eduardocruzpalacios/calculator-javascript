2c5cdeca012246f5da25793d7686c2da07be683e
educp <52535457+eduardocruzpalacios@users.noreply.github.com>
2021-06-19 13:37:43 +0200

    + - * / operators

--- app.js
+++ app.js
@@ -1,14 +1,57 @@
 // VARIABLES
-var result;
-var input;
-var started;
 
-// USER CLICS AN OPERATOR 1ST TIME --> INPUT GOES TO RESULT AND SAVE THE OPERATOR (+ - / X)
+const userInput = document.getElementById('input');
+const userResult = document.getElementById('result');
 
-// USER CLICS AN OPERATOR 2NDO AND BEYOND TIME --> DEPENDING ON OPERATOR, THE APP DO THE OPERATION WITH RESULT AND INPUT AND SAVE THE OPERATOR
-// WE CONTROL THE XTH TIME WITH A SWITCH (STARTED)
+const operators = document.getElementsByClassName('operator');
 
-// USER CLICS AN OPERATOR AFTER CLICING ANOTHER OPERATOR AND WITH NO INPUT VALUE
-// MAYBE AN ALERT OR AN ERROR MESSAGE
+var result = 0;
+var value;
+var started = false;
+var operator;
+var operatorsSymbols = ['/', '*', '-', '+'];
+
+console.log('operators: ' + operators);
+
+for (let i = 0; i < operators.length; i++) {
+
+    operators[i].addEventListener('click', function () {
+
+        value = Number(userInput.value);
+
+        if (value == "") {
+            // CLIC AND INPUT NOT FOUND
+            alert('Write some number before calculating');
+        } else if (!started) {
+            // CLIC OPERATOR 1ST TIME -> INPUT GOES TO RESULT, SAVE OPERATOR, CHANGE SWITCH
+            result = value;
+            started = true;
+        } else {
+            // CLICS OPERATOR 2NDO AND BEYOND TIME -> OPERATE, SAVE OPERATOR
+            switch (operator) {
+                case '+':
+                    result += value;
+                    break;
+                case '-':
+                    result -= value;
+                    break;
+                case '*':
+                    result *= value;
+                    break;
+                case '/':
+                    result /= value;
+                    break;
+            }
+        }
+
+        operator = operators[i].value;
+        userInput.value = '';
+
+        console.log('result: ' + result);
+        console.log('operator: ' + operator);
+
+    }, false);
+
+}
 
 // USER CLICS = --> DEPENDING ON OPERATOR, THE APP DO THE OPERATION WITH RESULT AND INPUT; THEN, RETURN RESULT
