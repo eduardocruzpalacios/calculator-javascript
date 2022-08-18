b79805110b461a76f79255a6ceeffb7a2ac61db1
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-08-18 22:21:32 +0200

    Create Calculator class

---
+++ calculator/calculator.js
@@ -0,0 +1,58 @@
+export class Calculator {
+  constructor() {
+    this.operand = '';
+    this.operator = null;
+    this.computation = 0;
+  }
+
+  assignOperand(operand) {
+    if (this.computation === 0 || operand === '') {
+      this.operand = operand;
+    } else {
+      this.operand += operand;
+    }
+    if (this.operator === null) {
+      this.computation = Number(this.operand);
+    }
+  }
+
+  setOperator(operator) {
+    if (this.operand === '' && this.computation === 0) {
+      return;
+    }
+    this.operator = operator;
+    this.operand = '';
+  }
+
+  getCurrentOperand() {
+    return this.operand;
+  }
+
+  compute() {
+    if (this.operand === '') {
+      return;
+    }
+    switch (this.operator) {
+      case '+':
+        this.computation += Number(this.operand);
+        break;
+      case '-':
+        this.computation -= Number(this.operand);
+        break;
+      case '*':
+        this.computation *= Number(this.operand);
+        break;
+      case '/':
+        this.computation /= Number(this.operand);
+        break;
+      default:
+        return;
+    }
+    this.operand = '';
+    this.operator = null;
+  }
+
+  getComputation() {
+    return this.computation;
+  }
+}
